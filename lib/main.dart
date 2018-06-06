1f48ec0ff1d8a96a3f7a386b68fb27926e1b78f3
JoyalAJohney <joyalajohny@gmail.com>
2018-06-06 18:23:36 +0530

    Transfered files between Internet using JSON and Parsed it

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,27 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+class  Post{
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  Post({this.userId,this.id,this.title,this.body});
+
+  factory Post.fromJson(Map<String, dynamic> json){
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
+
 
 
 void main() => runApp(RealWorldApp());
@@ -11,17 +33,15 @@ class RealWorldApp extends StatefulWidget {
 
 class _RealWorldAppState extends State<RealWorldApp> {
 
-  var _isLoading = true;
+  // var _isLoading = true;
 
-  _fetchData() async {
-    print("Attempting to fetch data from internet");
-    final String url = "http://api.letsbuildthatapp.com/youtube/home_feed";
-    final response = await http.get(url);
-    
-    if (response.statusCode == 200){
-      print(response.body);
-    }
+  Future<Post> fetchPost() async {
+    final response = await http.get('https://jsonplaceholder.typicode.com/posts/1');
+    final responseJson = json.decode(response.body);
+
+    return Post.fromJson(responseJson);
   }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -34,16 +54,27 @@ class _RealWorldAppState extends State<RealWorldApp> {
               icon: Icon(Icons.refresh),
               onPressed: (){
                 print("Reloading....");
-                setState(() {
-                  _isLoading = false;                
-                });
-                _fetchData();
+                // setState(() {
+                  // _isLoading = false;                
+                // });
+                // _fetchData();
               },
             ),
           ],
         ),
         body: Center(
-          child: _isLoading ? CircularProgressIndicator() : Text("Finished Loading...."),
+          child: FutureBuilder<Post>(
+            future: fetchPost(),
+            builder: (context,snapshot){
+              if(snapshot.hasData){
+                return Text(snapshot.data.title);
+              }else if (snapshot.hasError){
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          )
         ),
       ),
     );
