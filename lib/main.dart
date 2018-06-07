c956ac2307a534252e068bc31a0a380e5f511489
JoyalAJohney <joyalajohny@gmail.com>
2018-06-07 11:54:52 +0530

    Firebase packages added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() => runApp(MaterialApp(
   theme: ThemeData(
