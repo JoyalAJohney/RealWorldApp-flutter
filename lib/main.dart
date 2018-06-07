31c298e134b9c9c00bad836ea4e95cfd56b9d706
JoyalAJohney <joyalajohny@gmail.com>
2018-06-07 10:47:30 +0530

    Add Sign in/out Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,42 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.black,
+    accentColor: Colors.red,
+  ),
+  home: RealWorldApp(),
+));
 
 
-class  Post{
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userId,this.id,this.title,this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json){
-    return Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
-
-
-
-void main() => runApp(RealWorldApp());
-
-class RealWorldApp extends StatefulWidget {
-  @override
-  _RealWorldAppState createState() => _RealWorldAppState();
-}
-
-class _RealWorldAppState extends State<RealWorldApp> {
-
-  // var _isLoading = true;
-
-  Future<Post> fetchPost() async {
-    final response = await http.get('https://jsonplaceholder.typicode.com/posts/1');
-    final responseJson = json.decode(response.body);
-
-    return Post.fromJson(responseJson);
-  }
-  
-
+class RealWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Real World App"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.refresh),
-              onPressed: (){
-                print("Reloading....");
-                // setState(() {
-                  // _isLoading = false;                
-                // });
-                // _fetchData();
-              },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Real World App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: null,
+              child: Text("Sign In"),
+              color: Colors.green,
             ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+            ),
+            RaisedButton(
+              onPressed: null,
+              child: Text("Sign Out"),
+              color: Colors.blue,
+            )
           ],
-        ),
-        body: Center(
-          child: FutureBuilder<Post>(
-            future: fetchPost(),
-            builder: (context,snapshot){
-              if(snapshot.hasData){
-                return Text(snapshot.data.title);
-              }else if (snapshot.hasError){
-                return Text("${snapshot.error}");
-              }
-
-              return CircularProgressIndicator();
-            },
-          )
         ),
       ),
     );
