96884aac10efcb26a9cff8a38fd90ceac5e68224
JoyalAJohney <joyalajohny@gmail.com>
2018-06-29 10:07:56 +0530

    ProductDisplay initialised

---
+++ lib/productDisplay.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProductDisplay extends StatefulWidget {
+  @override
+  _ProductDisplayState createState() => _ProductDisplayState();
+}
+
+class _ProductDisplayState extends State<ProductDisplay> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
